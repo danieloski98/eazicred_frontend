bc2b3215690bd41504d48874558823e459c49d75
Neon <danielemmanuel257@gmail.com>
2021-08-25 17:45:08 +0100

    changed the url

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,6 +1,5 @@
 const local = 'http://localhost:4000';
 const live = 'https://api.eazicred.com';
 
-const local2 = 'https://109e-197-210-55-73.ngrok.io';
 
-export const URL = local;
+export const URL = live;
